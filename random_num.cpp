702ddfc3adfdf47515a12a338ef4a1faea28cf37
mahesh <maheshshirame07@gmail.com>
2026-09-01 16:42:16 +0530

    modified

---
+++ random_num.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "=============================\n";
+    cout << "     NUMBER GUESSING GAME\n";
+    cout << "=============================\n";
+    cout << "I have chosen a number between 1 and 100.\n";
+
+    do {
+        cout << "\nEnter your guess: ";
+        cin >> guess;
+
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too High!\n";
+        }
+        else if (guess < secretNumber) {
+            cout << "Too Low!\n";
+        }
+        else {
+            cout << "\nCongratulations! You won! \n";
+            cout << "You guessed the number in "
+                 << attempts << " attempts.\n";
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
